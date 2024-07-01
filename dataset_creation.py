9fb5db87a11191e86408b79def16e3ee9733a33f
GGhislandi <giulio.ghislandi@itsrizzoli.it>
2024-07-01 19:53:35 +0200

    creazione dataset standard

--- dataset_creation.py
+++ dataset_creation.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import shutil
 
 dataset_directory = "./datasets/crop_part1"
 
@@ -45,25 +46,34 @@ def extract_info_from_name(directory):
     df["ethnicity"] = [ethnicity_mapping[value] for value in ethnicities]
 
     df = df[df["age"] < 100]
-    df = df[df["ethnicity"] == "White"]
 
-    fasce = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 100]
+    fasce = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 80, 100]
     labels = ['0-4', '5-9', '10-14', '15-19', '20-24', '25-29', '30-34', '35-39', 
-          '40-44', '45-49', '50-54', '55-59', '60-64', '65-69', '70+']
+          '40-44', '45-49', '50-54', '55-59', '60-64', '65-69', '70-79', '80+']
 
     # creazione nuova colonna
     df['age_group'] = pd.cut(df['age'], bins=fasce, labels=labels, right=False)
 
-    [print(f"{age_group}, {len(df[df["age_group"] == age_group])}") for age_group in df["age_group"].unique()]
-
-    df.to_csv("data.csv")
+    return df
 
 def save_dataset(directory, df: pd.DataFrame):
 
-    for feature in df.columns.values:
-        print(feature)
+    for age_group in df.age_group.unique():
+        os.mkdir(f"{directory}/{age_group}")
 
+        subset = df[df["age_group"] == age_group]
+
+        for file in subset["file"]:
+            shutil.copy(f"./datasets/crop_part1/{file}", f"./datasets/ds_standard/{age_group}/{file}")
+
+def count_dataset(directory):
+
+    [print(cartella, len(os.listdir(directory+"/"+cartella))) for cartella in os.listdir(directory)]
 
 
 if __name__ == "__main__":
-    extract_info_from_name(dataset_directory)
+    df = extract_info_from_name(dataset_directory)
+
+    save_dataset("./datasets/ds_standard", df)
+
+    count_dataset("./datasets/ds_standard")
