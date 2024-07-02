01e45e848ef9ace39fb9f61bde40d74fb723d319
GGhislandi <giulio.ghislandi@itsrizzoli.it>
2024-07-02 14:28:21 +0200

    pulizia codice pre-processing, training e visualization

--- gender-and-age-prediction-from-face.py
+++ gender-and-age-prediction-from-face.py
@@ -16,19 +16,9 @@ import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
 import os
 
-for dirname, _, filenames in os.walk('datasets/crop_part1'):
-
-    for filename in filenames:
-        print(os.path.join(dirname, filename))
-
-# You can write up to 20GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All" 
-# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
-
-
-# # Import Libraries
-
-# In[13]:
-
+# impostazione della cartella di esecuzione corretta
+script_dir = os.path.dirname(__file__)
+os.chdir(script_dir)
 
 import numpy as np
 import pandas as pd
@@ -46,27 +36,22 @@ import tensorflow as tf
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dropout, Dense
 from tensorflow.keras.preprocessing.image import load_img
-from tensorflow.keras.utils import plot_model
+#from tensorflow.keras.utils import plot_model
 
-import warnings
-warnings.filterwarnings("ignore")
+###
+### Preprocessing
+###
 
-
-# # Load Data
-
-# In[14]:
-
+# caricamento dataset
 
 root_dir = "datasets/crop_part1"
 
-
-# In[15]:
-
-
+# inizializzazione array nomi file, età e generi
 path_arr = []
 age_arr = []
 gender_arr = []
 
+# estrazione dati dai nomi dei file
 for file in tqdm(os.listdir(root_dir)):
     labels = file.split("_")
     
@@ -79,26 +64,26 @@ for file in tqdm(os.listdir(root_dir)):
     gender_arr.append(gender)
 
 
-# In[16]:
-
-
+# inizializzazione dataframe con gli array dei dati
 df = pd.DataFrame({
     "image_path": path_arr,
     "age": age_arr,
     "gender": gender_arr
 })
 
+# shuffle del dataframe
+df_shuffled = df.sample(frac=1, random_state=42).reset_index(drop=True)
 
-# In[17]:
+# definizione delle percentuale di split
+train_frac = 0.6
+valid_frac = 0.2
 
+# calcolo indici per lo splitting
+train_idx = int(train_frac * len(df))
+valid_idx = int((train_frac + valid_frac) * len(df))
 
-df.head()
-
-
-# In[18]:
-
-
-df_train, df_valid, df_test = np.split(df.sample(frac=1), [int(.6*len(df)), int(.8*len(df))])
+# divisione dataframe in train, val e test
+df_train, df_valid, df_test = np.split(df_shuffled, [train_idx, valid_idx])
 
 df_train = df_train.reset_index(drop=True)
 df_valid = df_valid.reset_index(drop=True)
@@ -109,75 +94,57 @@ print("Train Size:", df_train.shape[0])
 print("valid Size:", df_valid.shape[0])
 print("Test Size:", df_test.shape[0])
 
+###
+### Data Visualization
+###
 
-# # Visualization
+## training
 
-# ### Train
-
-# In[19]:
-
-
+# creazione grafici distribuzione del genere
 plt.figure(figsize=(10, 8))
 ax = sns.countplot(x="gender", order=df_train["gender"].value_counts().index, data=df_train)
 ax.set_title("Train - Gender")
 for container in ax.containers:
     ax.bar_label(container)
 
-
-# In[20]:
-
-
+# creazione grafici distribuzione dell'età 
 plt.figure(figsize=(10, 8))
-ax = sns.distplot(x=df_train["age"])
-ax.set_title("Train - Gender")
+ax = sns.displot(x=df_train["age"])
+ax.set_title("Train - Age")
 plt.show()
 
+## validation
 
-# ### Valid
-
-# In[21]:
-
-
+# creazione grafici distribuzione dell'età
 plt.figure(figsize=(10, 8))
 ax = sns.countplot(x="gender", order=df_valid["gender"].value_counts().index, data=df_valid)
 ax.set_title("Valid - Gender")
 for container in ax.containers:
     ax.bar_label(container)
 
-
-# In[22]:
-
-
+# creazione grafici distribuzione dell'età 
 plt.figure(figsize=(10, 8))
-ax = sns.distplot(x=df_valid["age"])
-ax.set_title("Valid - Gender")
+ax = sns.displot(x=df_valid["age"])
+ax.set_title("Valid - Age")
 plt.show()
 
+## Test
 
-# ### Test
-
-# In[23]:
-
-
+# creazione grafici distribuzione dell'età
 plt.figure(figsize=(10, 8))
 ax = sns.countplot(x="gender", order=df_test["gender"].value_counts().index, data=df_test)
 ax.set_title("Test - Gender")
 for container in ax.containers:
     ax.bar_label(container)
 
-
-# In[24]:
-
-
+# creazione grafici distribuzione dell'età 
 plt.figure(figsize=(10, 8))
-ax = sns.distplot(x=df_test["age"])
-ax.set_title("Test - Gender")
+ax = sns.displot(x=df_test["age"])
+ax.set_title("Test - Age")
 plt.show()
 
 
-# In[25]:
-
-
+# visualizzazione di un sample del df
 def visualize_df(df: np.ndarray):
     fig, axes = plt.subplots(4, 4, figsize=(12, 12))
 
@@ -200,16 +167,11 @@ def visualize_df(df: np.ndarray):
     plt.tight_layout()
     plt.show()
 
-
-# In[26]:
-
-
 visualize_df(df_train)
 
 
-# In[34]:
 
-
+# inizializzazione array del training
 train_img_arr = []
 train_gender_arr = []
 train_age_arr = []
@@ -228,9 +190,7 @@ train_gender_arr = np.array(train_gender_arr)
 train_age_arr = np.array(train_age_arr)
 
 
-# In[27]:
-
-
+# inizializzazione array del validation
 valid_img_arr = []
 valid_gender_arr = []
 valid_age_arr = []
@@ -249,9 +209,7 @@ valid_gender_arr = np.array(valid_gender_arr)
 valid_age_arr = np.array(valid_age_arr)
 
 
-# In[28]:
-
-
+# inizializzazione array del test
 test_img_arr = []
 test_gender_arr = []
 test_age_arr = []
@@ -270,55 +228,66 @@ test_gender_arr = np.array(test_gender_arr)
 test_age_arr = np.array(test_age_arr)
 
 
-# # Model
+### 
+### Model
+###
 
-# In[29]:
+from tensorflow.keras.models import Sequential
 
+convolution = Sequential([
+    Input(shape=(128, 128, 1)),
 
+    Conv2D(32, kernel_size=(3, 3), activation='relu'),
+    MaxPooling2D(pool_size=(2, 2)),
+
+    Conv2D(64, kernel_size=(3, 3), activation='relu'),
+    MaxPooling2D(pool_size=(2, 2)),
+    
+    Conv2D(128, kernel_size=(3, 3), activation='relu'),
+    MaxPooling2D(pool_size=(2, 2)),
+
+    Flatten()
+])
+
+gender_branch = Sequential([
+    Dense(256, activation='relu'),
+    Dropout(0.3),
+    Dense(1, activation='sigmoid')
+])
+
+# Define the age branch
+age_branch = Sequential([
+    Dense(256, activation='relu'),
+    Dropout(0.3),
+    Dense(1, activation='relu')
+])
+
+# creazione dell'input
 inputs = Input(shape=(128, 128, 1))
 
-conv1 = Conv2D(32, kernel_size=(3, 3), activation='relu')(inputs)
-max1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+# passaggio attraverso la convoluzione
+flattened_image = convolution(inputs)
 
-conv2 = Conv2D(64, kernel_size=(3, 3), activation='relu')(max1)
-max2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+# passaggio attraverso i due branch
+output_gender = gender_branch(flattened_image)
+output_age = age_branch(flattened_image)
 
-conv3 = Conv2D(128, kernel_size=(3, 3), activation='relu')(max2)
-max3 = MaxPooling2D(pool_size=(2, 2))(conv3)
-
-flat = Flatten()(max3)
-
-dense1 = Dense(256, activation='relu')(flat)
-drop1 = Dropout(0.3)(dense1)
-output_gender = Dense(1, activation='sigmoid')(drop1)
-
-dense2 = Dense(256, activation='relu')(flat)
-drop2 = Dropout(0.3)(dense2)
-output_age = Dense(1, activation='relu')(drop2)
-
+# definizione modello
 model = Model(inputs=[inputs], outputs=[output_gender, output_age])
 
 model.compile(loss=['binary_crossentropy', 'mae'],
               optimizer='adam',
               metrics=['accuracy', 'mae'])
 
-
-# In[30]:
-
-
+# stampa del modello
 model.summary()
 
-
-# In[35]:
-
-
+# allenamento del modello
 history = model.fit(x=train_img_arr, y=[train_gender_arr, train_age_arr], batch_size=64, epochs=25, validation_data=(valid_img_arr, [valid_gender_arr, valid_age_arr]))
 
-
-# # Plot the Results
-
-# In[ ]:
-
+###
+### Test & Risultati
+###
 
 history_df = pd.DataFrame(history.history)
 history_df.head()
@@ -381,7 +350,7 @@ df_test.head()
 
 
 def visualize_results(df: pd.DataFrame):
-    fig, axes = plt.subplots(4, 4, figsize=(12, 12))
+    fig, axes = plt.subplots(16, 16, figsize=(12, 12))
 
     for i, ax in enumerate(axes.ravel()):
         if i < len(df):
